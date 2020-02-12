350fd856c0aaf428e376cb23b40866027bc975d1
Anthony Longo <alongo5@binghamton.edu>
2020-02-12 17:06:07 -0500

    wrote validation for argv values

--- submit/prj1-sol/wordcounts.cc
+++ submit/prj1-sol/wordcounts.cc
@@ -1,5 +1,26 @@
 #include <iostream>
 
 int main(int argc, char *argv[]){
-	return 0;
+	if(argc < 4){
+		std::cout << "Invalid number of arguments" << std::endl;
+		return(0);
+	}
+	int MAX_N_OUT = argv[1];
+	int MIN_WORD_LEN = argv[2];
+	int MAX_WORD_LEN = argv[3];
+	
+
+	//validate inputs
+	if(MAX_N_OUT != int(MAX_N_OUT)){
+		std::cout << "bad integer value " << MAX_N_OUT << " for MAX_N_OUT" << endl;
+		return(0);
+	}
+	else if(MIN_WORD_LEN != int(MIN_WORD_LEN)){
+		std::cout << "bad integer value " << MIN_WORD_LEN << " for MIN_WORD_LEN" << endl;
+		return(0);
+	}
+	else if(MAX_WORD_LEN != int(MAX_WORD_LEN)){
+		std::cout << "bad integer value " << MAX_WORD_LEN << " for MAX_WORD_LEN" << endl;
+	}
+
 }
