839388ab92af816c54e6cd139ecf5e0f7e4a0e46
= <=>
2020-02-14 23:12:30 -0500

    wrote basic input validation

--- submit/prj1-sol/wordcounts.cc
+++ submit/prj1-sol/wordcounts.cc
@@ -1,26 +1,29 @@
 #include <iostream>
+#include <cstdlib>
 
 int main(int argc, char *argv[]){
 	if(argc < 4){
 		std::cout << "Invalid number of arguments" << std::endl;
 		return(0);
 	}
-	int MAX_N_OUT = argv[1];
-	int MIN_WORD_LEN = argv[2];
-	int MAX_WORD_LEN = argv[3];
+	//FIX
+	//this still compiles if args are not integers
+	int MAX_N_OUT = std::atoi(argv[1]);
+	int MIN_WORD_LEN = std::atoi(argv[2]);
+	int MAX_WORD_LEN = std::atoi(argv[3]);
 	
 
 	//validate inputs
 	if(MAX_N_OUT != int(MAX_N_OUT)){
-		std::cout << "bad integer value " << MAX_N_OUT << " for MAX_N_OUT" << endl;
+		std::cout << "bad integer value " << MAX_N_OUT << " for MAX_N_OUT" << std::endl;
 		return(0);
 	}
 	else if(MIN_WORD_LEN != int(MIN_WORD_LEN)){
-		std::cout << "bad integer value " << MIN_WORD_LEN << " for MIN_WORD_LEN" << endl;
+		std::cout << "bad integer value " << MIN_WORD_LEN << " for MIN_WORD_LEN" << std::endl;
 		return(0);
 	}
 	else if(MAX_WORD_LEN != int(MAX_WORD_LEN)){
-		std::cout << "bad integer value " << MAX_WORD_LEN << " for MAX_WORD_LEN" << endl;
+		std::cout << "bad integer value " << MAX_WORD_LEN << " for MAX_WORD_LEN" << std::endl;
 	}
 
 }
