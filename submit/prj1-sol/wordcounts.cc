b5cd1f9566effd32c90cafe6a6e661ce847ba368
Anthony Longo <alongo5@binghamton.edu>
2020-02-17 18:22:37 -0500

    finished input validation for wordcounts

--- submit/prj1-sol/wordcounts.cc
+++ submit/prj1-sol/wordcounts.cc
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <vector>
 #include <utility>
+#include <cstring>
 
 typedef std::pair<std::string, int> WordCount;
 
@@ -18,64 +19,89 @@ bool wordCountCompare(const WordCount &a, const WordCount &b){
   }
 }
 
+bool fileExists(const char *fileName){
+  std::ifstream ifile(fileName);
+  if(!ifile){return false;}
+  return true;
+}
+
+bool legal_int(char *str){
+  for (size_t i = 0; i < strlen(str); i++){
+    if(isdigit(str[i]) == false){
+      return false;
+    }
+  }
+  return true;
+}
+
 
 int main(int argc, char *argv[]){
   //validate inputs and set values
+  if(argc == 1){
+    std::cout << "usage: ./wordcounts MAX_N_OUT MIN_WORD_LEN MAC_WORD_LEN FILE..." << std::endl;
+    return 0;
+  }
   if(argc < 4){
     std::cout << "Invalid number of arguments" << std::endl;
     return(0);
   }
-  /*for(int i = 0; i < sizeof(argv[1])-1; i++){
-    std::cout << argv[1][i] << std::endl;
-    if(std::isdigit(argv[1][i]) == false){
+  if(!legal_int(argv[1])){
       std::cout << "bad integer value " << argv[1] << " for MAX_N_OUT" << std::endl;
       return(0);
-    }
   }
 
-  for(int i = 0; i < sizeof(argv[2])-1; i++){
-    if(std::isdigit(argv[2][i]) == false){
+  
+  if(!legal_int(argv[2])){
       std::cout << "bad integer value " << argv[2] << " for MIN_WORD_LEN" << std::endl;
       return(0);
-    }
   }
   
-  for(int i = 0; i < sizeof(argv[3])-1; i++){
-    if(std::isdigit(argv[3][i]) == false){
+  if(!legal_int(argv[3])){
       std::cout << "bad integer value " << argv[3] << " for MAX_WORD_LEN" << std::endl;
       return(0);
-    }
-  }*/
+  }
 	
-  int MAX_N_OUT = std::stoi(argv[1]);
-  int MIN_WORD_LEN = std::stoi(argv[2]);
-  int MAX_WORD_LEN = std::stoi(argv[3]);
+  size_t MAX_N_OUT = std::stoi(argv[1]);
+  size_t MIN_WORD_LEN = std::stoi(argv[2]);
+  size_t MAX_WORD_LEN = std::stoi(argv[3]);
+
+  if(MIN_WORD_LEN > MAX_WORD_LEN){
+    std::cout << "MIN_WORD_LEN " << MIN_WORD_LEN << " is greater than MAX_WORD_LEN " << MAX_WORD_LEN << std::endl;
+    return 0;
+  }
   std::unordered_map<std::string, int> wordmap;
   
   //iterate through files
   for(int i = 4; i < argc; i ++){
-    std::ifstream in(argv[i]);
-    while (in.good()){
-      std::string w;
-      in >> w;
-      std::string formatted_w;
-      //reformat word
-      for(std::string::size_type letter = 0; letter< w.size(); letter++){
-	if(std::isalpha(w[letter])){
-	  formatted_w += std::tolower(w[letter]);
-	}
+    if(fileExists(argv[i])){
+      std::ifstream in(argv[i]);
+      while (in.good()){
+        std::string w;
+        in >> w;
+        std::string formatted_w;
+        //reformat word
+        for(std::string::size_type letter = 0; letter< w.size(); letter++){
+	  if(std::isalpha(w[letter])){
+	    formatted_w += std::tolower(w[letter]);
+	  }
+        }
+        //add word to map if in parameters
+        if(formatted_w.length()>=MIN_WORD_LEN && formatted_w.length()<=MAX_WORD_LEN){
+	  wordmap[formatted_w] += 1; 
+        }
       }
-      //add word to map if in parameters
-      if(formatted_w.length()>=MIN_WORD_LEN && formatted_w.length()<=MAX_WORD_LEN){
-	wordmap[formatted_w] += 1; 
-      }
+    }
+    else{
+      std::cout << "cannot read " << argv[i] << std::endl;
+      std::cout << "No such file or directory" << std::endl;
+      return 0; 
     }
   }
   //convert to vector and sort
   auto wordCounts = std::vector<WordCount>(wordmap.begin(), wordmap.end());
   sort(wordCounts.begin(), wordCounts.end(), wordCountCompare);
   //print vector items until MAX_N_OUT
-  for(int i = 0; i < MAX_N_OUT; i++){
+  for(size_t i = 0; i < MAX_N_OUT; i++){
     std::cout << wordCounts[i].first << ": " << wordCounts[i].second << std::endl;
   }
 }
