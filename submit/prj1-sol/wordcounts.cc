7fb0d3d34158fbd498c594c6dc79401081e7d346
Anthony Longo <alongo5@binghamton.edu>
2020-02-17 14:33:32 -0500

    worked on project

--- submit/prj1-sol/wordcounts.cc
+++ submit/prj1-sol/wordcounts.cc
@@ -1,26 +1,57 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
 
 int main(int argc, char *argv[]){
-	if(argc < 4){
-		std::cout << "Invalid number of arguments" << std::endl;
-		return(0);
-	}
-	int MAX_N_OUT = argv[1];
-	int MIN_WORD_LEN = argv[2];
-	int MAX_WORD_LEN = argv[3];
+  //validate inputs and set values
+  if(argc < 4){
+    std::cout << "Invalid number of arguments" << std::endl;
+    return(0);
+  }
+  /*for(int i = 0; i < sizeof(argv[1])-1; i++){
+    std::cout << argv[1][i] << std::endl;
+    if(std::isdigit(argv[1][i]) == false){
+      std::cout << "bad integer value " << argv[1] << " for MAX_N_OUT" << std::endl;
+      return(0);
+    }
+  }
+
+  for(int i = 0; i < sizeof(argv[2])-1; i++){
+    if(std::isdigit(argv[2][i]) == false){
+      std::cout << "bad integer value " << argv[2] << " for MIN_WORD_LEN" << std::endl;
+      return(0);
+    }
+  }
+  
+  for(int i = 0; i < sizeof(argv[3])-1; i++){
+    if(std::isdigit(argv[3][i]) == false){
+      std::cout << "bad integer value " << argv[3] << " for MAX_WORD_LEN" << std::endl;
+      return(0);
+    }
+  }*/
 	
-
-	//validate inputs
-	if(MAX_N_OUT != int(MAX_N_OUT)){
-		std::cout << "bad integer value " << MAX_N_OUT << " for MAX_N_OUT" << endl;
-		return(0);
+  int MAX_N_OUT = std::stoi(argv[1]);
+  int MIN_WORD_LEN = std::stoi(argv[2]);
+  int MAX_WORD_LEN = std::stoi(argv[3]);
+  std::unordered_map<std::string, int> wordmap;
+  for(int i = 4; i < argc; i ++){
+    std::ifstream in(argv[i]);
+    while (in.good()){
+      std::string w;
+      in >> w;
+      std::string formatted_w;
+      for(std::string::size_type letter = 0; letter< w.size(); letter++){
+	if(std::isalpha(w[letter])){
+	  formatted_w += std::tolower(w[letter]);
 	}
-	else if(MIN_WORD_LEN != int(MIN_WORD_LEN)){
-		std::cout << "bad integer value " << MIN_WORD_LEN << " for MIN_WORD_LEN" << endl;
-		return(0);
-	}
-	else if(MAX_WORD_LEN != int(MAX_WORD_LEN)){
-		std::cout << "bad integer value " << MAX_WORD_LEN << " for MAX_WORD_LEN" << endl;
-	}
-
+      }
+      //put thing here
+      if(formatted_w.length()>=MIN_WORD_LEN && formatted_w.length()<=MAX_WORD_LEN){
+	std::cout << formatted_w << std::endl;
+	wordmap[formatted_w] += 1; 
+	std::cout << wordmap[formatted_w] << std::endl;
+      }
+    }
+  }
 }
